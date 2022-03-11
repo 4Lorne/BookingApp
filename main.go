a33221f0be6d5a9cd54c3344630867c056e024a1
Lorne Day <lornedayy@gmail.com>
2022-03-10 22:57:47 -0400

    Initial Commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("Get your tickets here to attend")
+
+	conferenceTickets = 30
+	fmt.Println(conferenceTickets)
+}
